188a2b4656654a091b4630ba3cda1c530d39dd9a
Thulani <thulani.ncube21@yahoo.com>
2020-08-26 20:57:57 +0200

    Added more content

--- js/init.js
+++ js/init.js
@@ -7,7 +7,10 @@
     $('.modal').modal();
     $('.materialboxed').materialbox();
     $('select').formSelect();
-
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        duration: 20
+      });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
